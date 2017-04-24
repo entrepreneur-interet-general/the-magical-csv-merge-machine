856ab2d7198abcd4a1e2e5785d2341ada781ea57
lleeoo <leo@bouloc.eu>
2017-04-24 17:32:54 +0200

    Moved modules out of class definitions

---
+++ merge_machine/MODULES.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Apr 24 17:18:28 2017
+
+@author: leo
+"""
+
+from dedupe_linker import dedupe_linker
+from infer_nan import infer_mvs, replace_mvs, sample_mvs
+from performance import results_analysis
+from preprocess_fields_v3 import inferTypes, normalizeValues
+
+MODULES = {
+        'transform':{
+                    'INIT': {
+                                'desc': 'Initial upload (cannot be called)'                            
+                            },
+                    'replace_mvs': {
+                                'func': replace_mvs,
+                                'desc': replace_mvs.__doc__
+                            },
+                    'normalize': {
+                                'func':  normalizeValues,
+                                'desc': normalizeValues.__doc__
+                            }
+                    },
+        'infer':{
+                'infer_mvs': {
+                                'func': infer_mvs,
+                                'write_to': 'replace_mvs',
+                                'desc': infer_mvs.__doc__
+                            },
+               'inferTypes': {
+                                'func': inferTypes,
+                                'write_to': 'normalize',
+                                'desc': inferTypes.__doc__
+                            },
+                'results_analysis': {
+                                'func': results_analysis,
+                                'write_to': 'results_analysis',
+                                'desc': results_analysis.__doc__
+                            }
+                },
+        'link': {
+                'dedupe_linker': {
+                                'func': dedupe_linker,
+                                'desc': dedupe_linker.__doc__
+                            }
+                },
+                
+        'sample': {
+                'sample_mvs': {
+                        'func': sample_mvs,
+                        'desc': sample_mvs.__doc__
+                        }
+                }
+        }
