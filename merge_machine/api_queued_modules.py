4aa2577c0409d3e4fa470b36ac80ef10e4e3c519
lleeoo <leo@bouloc.eu>
2017-06-07 13:57:16 +0200

    added doc to queued modules

--- merge_machine/api_queued_modules.py
+++ merge_machine/api_queued_modules.py
@@ -14,12 +14,77 @@ os.chdir(curdir)
 
 from normalizer import UserNormalizer
 from linker import UserLinker
+    
+    
+def _infer_mvs(project_id, data_params, module_params):
+    '''
+    Runs the infer_mvs module
+    
+    wrapper around UserNormalizer.infer ?
+    
+    ARGUMENTS (GET):
+        project_id: ID for "normalize" project
+
+    ARGUMENTS (POST):
+        - data_params: {
+                "module_name": module to fetch from
+                "file_name": file to fetch
+                }
+        - module_params: none
+    
+    '''
+    proj = UserNormalizer(project_id=project_id)
+    proj.load_data(data_params['module_name'], data_params['file_name'])    
+    result = proj.infer('infer_mvs', module_params)
+        
+    # Write log
+    proj.write_log_buffer(False)
+    return result
+
+
+def _replace_mvs(project_id, data_params, module_params):
+    '''
+    Runs the mvs replacement module
+    
+    ARGUMENTS (GET):
+        project_id: ID for "normalize" project
+
+    ARGUMENTS (POST):
+        - data_params: {
+                "module_name": module to fetch from
+                "file_name": file to fetch
+                }
+        - module_params: same as result of infer_mvs
+    '''
+    proj = UserNormalizer(project_id=project_id)
+
+    proj.load_data(data_params['module_name'], data_params['file_name'])
+    
+    proj.transform('replace_mvs', module_params)
+    # Write transformations and log
+    proj.write_data()    
+    proj.write_log_buffer(True)
+    proj.write_run_info_buffer()
+    return
 
 
 def _concat_with_init(project_id, data_params, *argv):
-    '''Re-creates original file from '''
-    proj = UserNormalizer(project_id=project_id)
+    '''
+    Concatenate transformed columns with original file 
 
+    ARGUMENTS (GET):
+        project_id: ID for "normalize" project
+
+    ARGUMENTS (POST):
+        - data_params: file to concatenate to original
+                {
+                    "module_name": module to fetch from
+                    "file_name": file to fetch
+                }
+                
+        - module_params: none
+    '''
+    proj = UserNormalizer(project_id=project_id)
 
     # TODO: not clean
     if data_params is None:
@@ -39,44 +104,36 @@ def _concat_with_init(project_id, data_params, *argv):
     proj.write_run_info_buffer()
     return
 
+
 def _create_labeller(project_id, *argv):
+    '''
+    Create a "dedupe" labeller and pickle to project
+    
+    ARGUMENTS (GET):
+        - project_id
+    
+    ARGUMENTS (POST):
+        - data_params: none
+        - module_params: none
+    '''
+    
     # TODO: data input in gen_dedupe_labeller ?
     proj = UserLinker(project_id=project_id)
     labeller = proj._gen_dedupe_labeller()
     proj.write_labeller(labeller)
-    
-    
-def _infer_mvs(project_id, data_params, module_params):
-    '''
-    Runs the infer_mvs module
-    
-    wrapper around UserNormalizer.infer ?
-    
-    GET:
-        project_id: ID for "normalize" project
-
-    POST:
-        - data: {
-                "module_name": module to fetch from
-                "file_name": file to fetch
-                }
-        - params: parameters for the inference
-    
-    '''
-    proj = UserNormalizer(project_id=project_id)
-    proj.load_data(data_params['module_name'], data_params['file_name'])    
-    result = proj.infer('infer_mvs', module_params)
-        
-    # Write log
-    proj.write_log_buffer(False)
-    return result
-
 
 # In test_linker
 def _linker(project_id, *argv):
     '''
     Runs deduper module. Contrary to other modules, linker modules, take
     paths as input (in addition to module parameters)
+    
+    ARGUMENTS (GET):
+        project_id: ID for "link" project
+
+    ARGUMENTS (POST):
+        - data_params: none
+        - module_params: none
     '''  
     
     proj = UserLinker(project_id=project_id) # Ref and source are loaded by default
@@ -108,19 +165,6 @@ def _linker(project_id, *argv):
                              proj.mem_data_info['file_name'])
     print('Wrote data to: ', file_path)
 
-    return
-
-def _replace_mvs(project_id, data_params, module_params):
-    '''Runs the mvs replacement module'''
-    proj = UserNormalizer(project_id=project_id)
-
-    proj.load_data(data_params['module_name'], data_params['file_name'])
-    
-    proj.transform('replace_mvs', module_params)
-    # Write transformations and log
-    proj.write_data()    
-    proj.write_log_buffer(True)
-    proj.write_run_info_buffer()
     return
 
 
