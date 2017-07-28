ed9e4fdad361557130926738889e114c48035b4e
Leobouloc <leo.bou@gmail.com>
2017-07-28 18:29:16 +0200

    added recode_types

--- merge_machine/examples/infer_recode_types.py
+++ merge_machine/examples/infer_recode_types.py
@@ -18,7 +18,8 @@ HOST = '0.0.0.0:5000'
 
 # CSV file to recode
 file_path = '../local_test_data/source.csv'
-output_file_path = 'infer_types.json'
+inference_file_path_write = 'infer_types.json'
+output_file_path = 'source_recoded.csv'
 
 
 # =============================================================================
@@ -80,11 +81,66 @@ while (time.time() - start_time) <= 100:
     # Otherwise check again in 0.5 second
     time.sleep(0.5)
     
-
 # =============================================================================
 # Write output to file
 # =============================================================================
-
-with open(output_file_path, 'w') as w:
+# NB: Currently, infer_types returns many superflueous information. The interesting
+# information is in parsed_resp['result']['column_types']
+with open(inference_file_path_write, 'w') as w:
     json.dump(parsed_resp['result']['column_types'], w)
-print('Result of type inference written to:', output_file_path)
+print('Result of type inference written to:', inference_file_path_write)
+
+# =============================================================================
+# Schedule recode_type (module is still being worked on)
+# =============================================================================
+# data_params: information on the file to use as input for inference or transformation
+# NB: file_name and module_name to use as input did not change²
+data_params = {'module_name': module_name, 'file_name': file_name}
+# module_params: parameters to use for inference or transform (here: we use the
+# result of the previous inference step)
+
+module_params = parsed_resp['result']
+
+url = PROTOCOL + HOST + '/api/schedule/recode_types/{0}/'.format(project_id)
+body =  {'data_params': data_params, 'module_params': module_params}
+resp = requests.post(url, json=body)
+parsed_resp = json.loads(resp.content.decode())
+
+# =============================================================================
+# Wait for response for recode_types job
+# =============================================================================
+url = PROTOCOL + HOST + parsed_resp['job_result_api_url']
+start_time = time.time()
+# Wait for a maximimum of 100 seconds
+while (time.time() - start_time) <= 100:
+    resp = requests.get(url)
+    parsed_resp = json.loads(resp.content.decode())
+
+    # Check if job was completed
+    if parsed_resp['completed']:
+        break
+    
+    # Otherwise check again in 0.5 second
+    time.sleep(0.5)
+    
+# =============================================================================
+# Download the transformed file
+# =============================================================================
+url = PROTOCOL + HOST + '/api/download/normalize/{0}'.format(project_id)
+
+# Get the file_name and module_name to which the transformed file was written
+module_name = parsed_resp['result']['module_name']
+file_name = parsed_resp['result']['file_name']
+data_params = {'module_name': module_name, 'file_name': file_name}
+module_params = None
+
+body = {
+        'data_params': data_params,
+        'module_params': module_params
+        }
+resp = requests.post(url, json=body)
+
+with open(output_file_path,'wb') as w:
+    w.write(resp.content)
+    
+print('Result file of recode_types written to:', inference_file_path_write)
