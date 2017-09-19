6ccba1e91315c707450bceb93b22abeac81f467a
Leobouloc <leo.bou@gmail.com>
2017-09-19 13:37:51 +0200

    trying to fix issue with project listing

--- merge_machine/CONFIG.py
+++ merge_machine/CONFIG.py
@@ -6,7 +6,19 @@ Created on Fri Mar  3 10:41:34 2017
 @author: leo
 """
 
-DATA_PATH = 'data'
-LINK_DATA_PATH = 'data/link'
-NORMALIZE_DATA_PATH = 'data/normalize'
-RESOURCE_PATH = 'resource'
+import os 
+import sys
+
+cwd = os.getcwd()
+cwd = os.path.split((os.path.join(cwd, sys.argv[0])))[0]
+
+
+DATA_PATH = os.path.join(cwd, 'data')
+LINK_DATA_PATH = os.path.join(cwd, 'data/link')
+NORMALIZE_DATA_PATH = os.path.join(cwd, 'data/normalize')
+RESOURCE_PATH = os.path.join(cwd, 'resource')
+
+print('DATA_PATH\n', DATA_PATH)
+print('LINK_DATA_PATH\n', LINK_DATA_PATH)
+print('NORMALIZE_DATA_PATH\n', NORMALIZE_DATA_PATH)
+print('RESOURCE_PATH\n', RESOURCE_PATH)
