9afc4c31b65b284c40f9d1280815243880eeec3c
Leobouloc <leo.bou@gmail.com>
2017-11-06 13:26:29 +0100

    changed localhost

--- merge_machine/api.py
+++ merge_machine/api.py
@@ -1205,4 +1205,4 @@ def list_public_projects(project_type):
     return jsonify(list_of_projects)
 
 if __name__ == '__main__':
-    socketio.run(app, host='0.0.0.0', port=5000, debug=True)
+    socketio.run(app, host='127.0.0.1', port=5000, debug=True)
